36899c32f872df2431c256181174fcac11b4cbae
Joseph C <josephalexcobian@gmail.com>
2023-11-02 13:59:21 -0700

    Adjust behaviour of match checker so it is not triggered if confirm password is empty

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ let password=document.querySelector('#password');
 let confirmPassword=document.querySelector('#confirm-password');
 let passwordMismatch=document.querySelector('.password-mismatch')
 
-password.setAttribute('type', 'text');
-confirmPassword.setAttribute('type', 'text');
-
 function checkMatch(){
-    if (password.value!=confirmPassword.value) {
+    if (password.value!=confirmPassword.value && confirmPassword.value != '') {
         confirmPassword.classList.add('mismatch')
         password.classList.add('mismatch');
         passwordMismatch.style.display='block';
     }
-    else if (password.value==confirmPassword.value) {
+    else if (password.value==confirmPassword.value || confirmPassword.value == '') {
         confirmPassword.classList.remove('mismatch')
         password.classList.remove('mismatch');
         passwordMismatch.style.display='none';
